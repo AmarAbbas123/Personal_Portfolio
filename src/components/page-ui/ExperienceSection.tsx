dd3dc8833c8010a2a383b3f5657ee66590951d61
Amar Abbas <amarabbas1414@gmail.com>
2025-02-10 20:07:18 +0500

    Fix ESLint and Prettier conflicts

--- src/components/page-ui/ExperienceSection.tsx
+++ src/components/page-ui/ExperienceSection.tsx
@@ -1,107 +1,81 @@
-"use client";
-import React from "react";
-import { twMerge } from "tailwind-merge";
+'use client';
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
 
-import { TracingBeam } from "@/components/ui/tracing-beams";
+import { TracingBeam } from '@/components/ui/tracing-beams';
 
 export function ExperienceSection() {
-  return (
-    <>
-      <h1
-        id="experience"
-        className="mx-auto max-w-5xl px-8 pb-8 pt-20 text-2xl font-bold dark:text-white md:pt-32 md:text-7xl"
-      >
-        Experience
-      </h1>
-      <TracingBeam className="px-6">
-        <div className="relative mx-auto max-w-2xl pb-32 pt-4 text-white antialiased">
-          {experience.map((item, index) => (
-            <div key={`content-${index}`} className="mb-10 mt-4 md:mt-0">
-              <h2 className={twMerge("text-xl text-black dark:text-white")}>
-                {item.title}
-              </h2>
-              <span className="w-fit rounded-full py-1 text-sm italic text-neutral-800 dark:text-neutral-200">
-                {item.badge}
-              </span>
-              <div className="prose prose-sm dark:prose-invert mt-2 text-sm text-black dark:text-white">
-                {item.description}
-              </div>
-            </div>
-          ))}
-        </div>
-      </TracingBeam>
-    </>
-  );
+    return (
+        <>
+            <h1 id="experience" className="mx-auto max-w-5xl px-8 pb-8 pt-20 text-2xl font-bold dark:text-white md:pt-32 md:text-7xl">
+                Experience
+            </h1>
+            <TracingBeam className="px-6">
+                <div className="relative mx-auto max-w-2xl pb-32 pt-4 text-white antialiased">
+                    {experience.map((item, index) => (
+                        <div key={`content-${index}`} className="mb-10 mt-4 md:mt-0">
+                            <h2 className={twMerge('text-xl text-black dark:text-white')}>{item.title}</h2>
+                            <span className="w-fit rounded-full py-1 text-sm italic text-neutral-800 dark:text-neutral-200">{item.badge}</span>
+                            <div className="prose prose-sm dark:prose-invert mt-2 text-sm text-black dark:text-white">{item.description}</div>
+                        </div>
+                    ))}
+                </div>
+            </TracingBeam>
+        </>
+    );
 }
 
 const experience = [
-  {
-    title: "Secure Site Audit",
-    description: (
-      <ul className="list-disc">
-        <li>
-          Developed a secure web portal-based project management console to
-          manage site audits.
-        </li>
-        <li>
-          Contributed to the development of a web applicatigion by implementing
-          a new app theme and developing various frontend components.
-        </li>
-      </ul>
-    ),
-    badge: "Junior Software Engineer/Oct 2023 - Present",
-  },
-  {
-    title: "Alilogics",
-    description: (
-      <ul className="list-disc">
-        <li>Joined Alilogics as a Junior Frontend Developer.</li>
-        <li>
-          Developed and customized a wide range of WordPress projects, including
-          theme customization, Elementor-based designs, and WooCommerce
-          integrations, tailored to meet diverse client requirements and
-          expectations.
-        </li>
-      </ul>
-    ),
-    badge: "Frontend Developer/June 2023 – Present",
-  },
-  {
-    title: "SOFTOO (Internship)",
-    description: (
-      <ul className="list-disc">
-        <li>
-          Completed a MERN Stack Bootcamp at Softoo, gaining practical
-          experience in building full-stack applications with MongoDB,
-          Express.js, React, and Node.js
-        </li>
-        <li>
-          Developed a web app to connect patients with doctors for online
-          appointments
-        </li>
-        <li>
-          Implemented features for booking, rescheduling, and managing
-          appointments. Integrated secure communication channels and real- time
-          notifications for efficient management.
-        </li>
-      </ul>
-    ),
-    badge: "MERN Stack Trainee/Sep 2022 - Nov 2022",
-  },
-  {
-    title: "K2X Tech (Intenship)",
-    description: (
-      <ul className="list-disc">
-        <li>
-          Delivered a variety of mobile development projects, meeting diverse
-          client requirements and expectations.
-        </li>
-        <li>
-          Worked extensively with React, and vanilla JavaScript to develop
-          robust and scalable mobile applications
-        </li>
-      </ul>
-    ),
-    badge: "Frontend Development/June 2022 - Aug 2022",
-  },
+    {
+        title: 'Secure Site Audit',
+        description: (
+            <ul className="list-disc">
+                <li>Developed a secure web portal-based project management console to manage site audits.</li>
+                <li>
+                    Contributed to the development of a web applicatigion by implementing a new app theme and developing various frontend components.
+                </li>
+            </ul>
+        ),
+        badge: 'Junior Software Engineer/Oct 2023 - Present'
+    },
+    {
+        title: 'Alilogics',
+        description: (
+            <ul className="list-disc">
+                <li>Joined Alilogics as a Junior Frontend Developer.</li>
+                <li>
+                    Developed and customized a wide range of WordPress projects, including theme customization, Elementor-based designs, and
+                    WooCommerce integrations, tailored to meet diverse client requirements and expectations.
+                </li>
+            </ul>
+        ),
+        badge: 'Frontend Developer/June 2023 – Present'
+    },
+    {
+        title: 'SOFTOO (Internship)',
+        description: (
+            <ul className="list-disc">
+                <li>
+                    Completed a MERN Stack Bootcamp at Softoo, gaining practical experience in building full-stack applications with MongoDB,
+                    Express.js, React, and Node.js
+                </li>
+                <li>Developed a web app to connect patients with doctors for online appointments</li>
+                <li>
+                    Implemented features for booking, rescheduling, and managing appointments. Integrated secure communication channels and real- time
+                    notifications for efficient management.
+                </li>
+            </ul>
+        ),
+        badge: 'MERN Stack Trainee/Sep 2022 - Nov 2022'
+    },
+    {
+        title: 'K2X Tech (Intenship)',
+        description: (
+            <ul className="list-disc">
+                <li>Delivered a variety of mobile development projects, meeting diverse client requirements and expectations.</li>
+                <li>Worked extensively with React, and vanilla JavaScript to develop robust and scalable mobile applications</li>
+            </ul>
+        ),
+        badge: 'Frontend Development/June 2022 - Aug 2022'
+    }
 ];
--- src/components/page-ui/LandingPage.tsx
+++ src/components/page-ui/LandingPage.tsx
@@ -1,48 +1,48 @@
-"use client";
+'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
 
-import { BackgroundBeams } from "@/components/ui/background-beams";
-import { Button } from "@/components/ui/button";
-import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { BackgroundBeams } from '@/components/ui/background-beams';
+import { Button } from '@/components/ui/button';
+import { TypewriterEffectSmooth } from '@/components/ui/typewriter-effect';
 
 const words = [
-  {
-    text: "JavaScript",
-  },
-  {
-    text: "and",
-  },
-  {
-    text: "React/Next.js",
-  },
-  {
-    text: "developer",
-    className: "text-blue-500 dark:text-blue-500",
-  },
+    {
+        text: 'JavaScript'
+    },
+    {
+        text: 'and'
+    },
+    {
+        text: 'React/Next.js'
+    },
+    {
+        text: 'developer',
+        className: 'text-blue-500 dark:text-blue-500'
+    }
 ];
 
 export function LandingPage() {
-  return (
-    <div
-      id="landing-page"
-      className="bg- relative flex  h-screen w-full flex-col items-center justify-center bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]"
-    >
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)] dark:bg-black" />
-      <p className="text-xs text-neutral-600 dark:text-neutral-200 sm:text-base  ">
-        {" "}
-        Hello, I&apos;m Amar Abbas. A passionate Software Engineer.
-      </p>
-      <TypewriterEffectSmooth words={words} />
-      <div className="z-30 flex flex-col items-center space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-        <Link href="https://drive.google.com/file/d/1V4Hw_JkVoZb6M0PWxoPyyq4u1eW2AF8p/view?usp=drive_link">
-          <Button>Download CV</Button>
-        </Link>
-        <Link href="#contact-me">
-          <Button variant="secondary">Contact Me</Button>
-        </Link>
-      </div>
-      <BackgroundBeams />
-    </div>
-  );
+    return (
+        <div
+            id="landing-page"
+            className="bg- relative flex  h-screen w-full flex-col items-center justify-center bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]"
+        >
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)] dark:bg-black" />
+            <p className="text-xs text-neutral-600 dark:text-neutral-200 sm:text-base  ">
+                {' '}
+                Hello, I&apos;m Amar Abbas. A passionate Software Engineer.
+            </p>
+            <TypewriterEffectSmooth words={words} />
+            <div className="z-30 flex flex-col items-center space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+                <Link href="https://drive.google.com/file/d/1V4Hw_JkVoZb6M0PWxoPyyq4u1eW2AF8p/view?usp=drive_link">
+                    <Button>Download CV</Button>
+                </Link>
+                <Link href="#contact-me">
+                    <Button variant="secondary">Contact Me</Button>
+                </Link>
+            </div>
+            <BackgroundBeams />
+        </div>
+    );
 }
