d0a6d2fc32b52d9a03e963f25fbb3c793405531d
Amar Abbas <amarabbas1414@gmail.com>
2025-02-10 19:26:35 +0500

    first comment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['aceternity.com', 'images.unsplash.com', 'assets.aceternity.com'],
-  },};
+};
 
 export default nextConfig;
